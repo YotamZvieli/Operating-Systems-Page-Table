9eca2c82e074f16409706955a6b968746f7c3d12
yotamzvieli <yotamzvieli@mail.tau.ac.il>
2023-04-02 20:31:19 +0300

    finish pt task

---
+++ pt.c
@@ -0,0 +1,70 @@
+#include "os.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+uint64_t pross_level(uint64_t* current_pos, uint64_t address){
+    if(current_pos[address] == 0x0){
+        current_pos[address] = (alloc_page_frame() << 12) | 1;
+        return current_pos[address];
+    }
+    else{
+        return current_pos[address];
+    }
+}
+
+uint64_t* page_walk (uint64_t pt, uint64_t vpn_trans){
+    uint64_t vr1 = vpn_trans & 0xfffffffff;
+    uint64_t vr2 = vpn_trans & 0xfffffff;
+    uint64_t vr3 = vpn_trans & 0x3ffff;
+    uint64_t curr = pt;
+    for(int i = 1; i < 5; i++){
+        if( i == 1){
+            curr = pross_level((uint64_t *)phys_to_virt(curr),vpn_trans >> 36);
+        }
+        if (i == 2)
+        {
+            curr = pross_level((uint64_t *)phys_to_virt(curr),vr1 >> 27);
+        }
+        if (i == 3)
+        {
+            curr = pross_level((uint64_t *)phys_to_virt(curr),vr2 >> 18);
+        }
+        if(i == 4){
+            curr = pross_level((uint64_t *)phys_to_virt(curr),vr3 >> 9);
+        } 
+    }
+    return phys_to_virt(curr);
+
+       
+                
+}
+uint64_t page_table_query(uint64_t pt, uint64_t vpn){
+    uint64_t full_pa = pt << 12;
+    uint64_t* level5;
+    level5 = page_walk(full_pa,vpn);
+    int PTE = (vpn & 0x1ff);
+    if((level5[PTE] & 1) == 0){
+        return NO_MAPPING;
+    }
+    else
+    {
+        return level5[PTE];
+    }
+    
+    
+}
+void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn){
+    uint64_t full_pa = pt << 12;
+    uint64_t* level5;
+    level5 = page_walk(full_pa,vpn);
+    uint64_t PTE = (vpn & 0x1ff);
+    if(ppn == NO_MAPPING){
+        level5[PTE] = 0x0;
+    }
+    else
+    {
+         level5[PTE] = (ppn);
+    }
+}
+
